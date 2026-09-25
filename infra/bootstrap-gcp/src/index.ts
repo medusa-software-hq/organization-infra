f9357d1fe5ef1ef70281c31c0d1f377b199cef75
Jakub Trzebiatowski <jakub.trzebiatowski@medusa.software>
2026-09-25 18:45:28 +0200

    Move the bootstrap stack's state to the GCS bucket
    Adopt the existing resources via temporary imports instead of copying
    the local state. Stop creating the stack implicitly, now that it's
    shared.

--- infra/bootstrap-gcp/src/index.ts
+++ infra/bootstrap-gcp/src/index.ts
@@ -21,7 +21,11 @@ const billingAccount = gcp.organizations.getBillingAccountOutput({
 });
 
 /** The random suffix of the root project ID. */
-const rootProjectSuffix = new random.RandomId("root-project-suffix", { byteLength: 4 });
+const rootProjectSuffix = new random.RandomId(
+  "root-project-suffix",
+  { byteLength: 4 },
+  { import: "9p8qbQ" }, // TODO: Remove the import
+);
 
 /** The root project. */
 export const rootProject = new gcp.organizations.Project(
@@ -34,18 +38,26 @@ export const rootProject = new gcp.organizations.Project(
     // Deleting the root of trust must be a deliberate, two-step change
     deletionPolicy: "PREVENT",
   },
-  { protect: true },
+  { protect: true, import: "projects/ms-root-f69f2a6d" }, // TODO: Remove the import
 );
 
 /** The Cloud Storage API in the root project. */
-const storageApi = new gcp.projects.Service("storage-api", {
-  project: rootProject.projectId,
-  service: "storage.googleapis.com",
-  disableOnDestroy: false,
-});
+const storageApi = new gcp.projects.Service(
+  "storage-api",
+  {
+    project: rootProject.projectId,
+    service: "storage.googleapis.com",
+    disableOnDestroy: false,
+  },
+  { import: "ms-root-f69f2a6d/storage.googleapis.com" }, // TODO: Remove the import
+);
 
 /** The random suffix of the state bucket name. */
-const stateBucketSuffix = new random.RandomId("state-bucket-suffix", { byteLength: 4 });
+const stateBucketSuffix = new random.RandomId(
+  "state-bucket-suffix",
+  { byteLength: 4 },
+  { import: "a21wdw" }, // TODO: Remove the import
+);
 
 /** The bucket holding this stack's Pulumi state. */
 const stateBucket = new gcp.storage.Bucket(
@@ -66,7 +78,12 @@ const stateBucket = new gcp.storage.Bucket(
     // Must never be zero; together with versioning, it allows recovering overwritten state
     softDeletePolicy: { retentionDurationSeconds: 7 * 24 * 60 * 60 },
   },
-  { dependsOn: [storageApi], protect: true, retainOnDelete: true },
+  {
+    dependsOn: [storageApi],
+    protect: true,
+    retainOnDelete: true,
+    import: "ms-root-pulumi-state-6b6d7077", // TODO: Remove the import
+  },
 );
 
 export const stateBucketUrl = pulumi.interpolate`gs://${stateBucket.name}`;
