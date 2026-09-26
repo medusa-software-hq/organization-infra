5d3058036cb8d60117086c1a0b83f379d201ed32
Jakub Trzebiatowski <jakub.trzebiatowski@medusa.software>
2026-09-26 18:08:25 +0200

    Export only the root project's ID from the bootstrap stack

--- infra/bootstrap-gcp/src/index.ts
+++ infra/bootstrap-gcp/src/index.ts
@@ -1,8 +1,9 @@
 import * as pulumi from "@pulumi/pulumi";
+import { rootProject } from "./rootProject.ts";
 import { stateBucket } from "./stateBucket.ts";
 import "./access.ts";
 import "./organizationPolicies.ts";
 
-export { rootProject } from "./rootProject.ts";
+export const rootProjectId = rootProject.projectId;
 
 export const stateBucketUrl = pulumi.interpolate`gs://${stateBucket.name}`;
